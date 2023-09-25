6ebf335faf6ddc76ed9801ab73d41fbec436814f
2021147521 <jjgs1235@yonsei.ac.kr>
2023-09-26 02:23:59 +0900

    VueSNS back

--- ch1/back/app.js
+++ ch1/back/app.js
@@ -8,33 +8,53 @@ const passportConfig = require("./passport");
 const session = require("express-session");
 const cookie = require("cookie-parser");
 const morgan = require("morgan");
+const hpp = require("hpp");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+
+const prod = process.env.NODE_ENV === "production";
 const usersRouter = require("./routes/user");
 const postRouter = require("./routes/post");
 const postsRouter = require("./routes/posts");
 const hashtagRouter = require("./routes/hashtag");
 
+dotenv.config();
 db.sequelize.sync({});
 passportConfig();
 
-app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "http://localhost:3080",
-    credentials: true,
-  })
-);
+if (prod) {
+  app.use(helmet());
+  app.use(hpp());
+  app.use(morgan("combined "));
+  app.use(
+    cors({
+      origin: "http://jjgssns.com",
+      credentials: true,
+    })
+  );
+} else {
+  app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: "http://localhost:3080",
+      credentials: true,
+    })
+  );
+}
+
 app.use("/", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookie("cookiesecret"));
+app.use(cookie(process.env.COOKIE_SECRETE));
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
-    secret: "cookiesecret",
+    secret: process.env.COOKIE_SECRETE,
     cookie: {
       httpOnly: true,
       secure: false,
+      domain: prod && ".jjgssns.com",
     },
   })
 );
@@ -50,6 +70,6 @@ app.use("/post", postRouter);
 app.use("/posts", postsRouter);
 app.use("/hashtag", hashtagRouter);
 
-app.listen(3085, () => {
+app.listen(prod ? process.end.PORT : 3085, () => {
   console.log(`백엔드 서버${3085}번에서 작동중`);
 });
