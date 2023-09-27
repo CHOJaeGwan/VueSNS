c8144a7ef7732ba3186aa37eea589b60f7227e23
2021147521 <jjgs1235@yonsei.ac.kr>
2023-09-27 15:51:59 +0900

    VueSNS front

--- ch1/front/components/PostForm.vue
+++ ch1/front/components/PostForm.vue
@@ -29,11 +29,7 @@
             :key="p"
             style="display: inline-block"
           >
-            <img
-              :src="`http://localhost:3085/${p}`"
-              :alt="p"
-              style="width: 200px"
-            />
+            <img :src="p" :alt="p" style="width: 200px" />
             <div>
               <button type="button" @click="onRemoveImage(i)">제거</button>
             </div>
