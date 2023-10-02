cf1f5d20791788ba7b981f3a8f18f09df9604448
2021147521 <jjgs1235@yonsei.ac.kr>
2023-10-02 14:06:28 +0900

    VueSNS lambda

--- ch1/lambda/index.js
+++ ch1/lambda/index.js
@@ -1,9 +1,7 @@
-const {
-  S3
-} = require("@aws-sdk/client-s3");
+const AWS = require("aws-sdk");
 const Sharp = require("sharp");
 
-const S3 = new S3({ region: "ap-northeast-2" });
+const S3 = new AWS.S3({ region: "ap-northeast-2" });
 
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
@@ -17,7 +15,7 @@ exports.handler = async (event, context, callback) => {
       // S3에서 이미지를 받아 옵니다.
       Bucket,
       Key,
-    });
+    }).promise();
     console.log("original", s3Object.Body.length);
     const resizedImage = await Sharp(s3Object.Body)
       .resize(800, 800, {
@@ -30,7 +28,7 @@ exports.handler = async (event, context, callback) => {
       Body: resizedImage,
       Bucket,
       Key: `thumb/${filename}`,
-    });
+    }).promise();
     console.log("put");
     return callback(null, `thumb/${filename}`);
   } catch (error) {
